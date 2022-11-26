202e16decb32f6e664213947f3359d34f8c8b972
Lander Gallastegi <landergallastegi@gmail.com>
2022-11-26 21:25:28 +0100

    Mas movimiento y gravedad

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
 
+    public InputActionReference moveAction;
     public CharacterController controller;
     public float speed = 1f;
     public Transform floorDetector;
@@ -13,6 +15,7 @@ public class PlayerMovement : MonoBehaviour {
     private bool inGround;
     private Vector3 lastPos;
     private Vector3 pendingVelocity = new Vector3(0, 0, 0);
+    private Vector2 moveDir = new Vector2(0, 0);
 
     public void AddVelocity(Vector3 velocity) {
         pendingVelocity += velocity;
@@ -23,24 +26,31 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     void Start() {
-        
+        moveAction.action.started += moveActionStarted;
+        //moveAction.action.stopped += moveActionStopped;
+    }
+
+    void moveActionStarted(InputAction.CallbackContext context) {
+        moveDir = context.ReadValue<Vector2>();
+    }
+
+    void moveActionStopped(InputAction.CallbackContext context) {
+        moveDir = Vector2.zero;
     }
 
     void FixedUpdate() {
         this.inGround = Physics.CheckSphere(this.floorDetector.position, this.floorDistance, this.floor);
-        float x = 0; //get x from input
-        float y = 0; //get y from input
         Vector3 v = GetVelocity();
         lastPos = transform.position;
-        v += Physics.gravity;
+        v += Physics.gravity * Time.fixedDeltaTime;
         v += pendingVelocity;
         if (inGround) {
             v.x = 0;
             v.y = 0;
             v.z = 0;
         }
-        v.x = x * speed;
-        v.y = y * speed;
+        v.x = moveDir.x * speed;
+        v.z = moveDir.y * speed;
         pendingVelocity = new Vector3(0, 0, 0);
         this.controller.Move(v * Time.fixedDeltaTime);
     }
