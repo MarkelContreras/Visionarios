7d3f584c6ce16fe05c7b4ffb9be082a7a3a39bc2
Lander Gallastegi <landergallastegi@gmail.com>
2022-11-28 15:19:18 +0100

    Movimiento full solucionado

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,14 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour {
 
     public InputActionAsset inputActionAsset;
-    private InputAction moveAction;
     public CharacterController controller;
+    public Transform camera;
     public float speed = 1f;
     public Transform floorDetector;
     public float floorDistance = 0.1f;
     public LayerMask floor;
 
+    private InputAction moveAction;
     private bool inGround;
     private Vector3 lastPos;
     private Vector3 pendingVelocity = new Vector3(0, 0, 0);
@@ -21,6 +22,7 @@ public class PlayerMovement : MonoBehaviour {
     private void Awake() {
         inputActionAsset.Enable();
         moveAction = inputActionAsset.FindAction("Move");
+        lastPos = transform.position;
     }
 
     public void AddVelocity(Vector3 velocity) {
@@ -46,8 +48,8 @@ public class PlayerMovement : MonoBehaviour {
             v.y = 0;
             v.z = 0;
         }
-        v.x = moveDir.x * speed;
-        v.z = moveDir.y * speed;
+        Vector3 m = (camera.rotation * new Vector3(moveDir.x * speed, 0, moveDir.y * speed)).normalized;
+        v += m;
         pendingVelocity = new Vector3(0, 0, 0);
         this.controller.Move(v * Time.fixedDeltaTime);
     }
