292d15e360baa8cb8e95129b5ba6ab3725d9fc64
Lander Gallastegi <landergallastegi@gmail.com>
2023-01-03 18:23:06 +0100

    Markel, sube el código sin errores de compilación

--- Assets/Scripts/SelectorNiveles.cs
+++ Assets/Scripts/SelectorNiveles.cs
@@ -29,6 +29,6 @@ public class SelectorNiveles : MonoBehaviour
 
     public void Salir()
     {
-        Scene.Manager.LoadScene("MenuPrincipal")
+        SceneManager.LoadScene("MenuPrincipal");
     }
 }
